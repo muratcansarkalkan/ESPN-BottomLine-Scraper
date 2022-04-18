53f1c17f3c07932b2e1db917c53d8381e06d7404
bigmurtaza <muratcansarkalkan@gmail.com>
2022-04-18 21:42:43 +0300

    Better look of output

--- README.md
+++ README.md
@@ -6,10 +6,18 @@ It can scrape the box scores, major stats, cautionary print for game links and h
 <ul>
   <li>2015: Initial commit.</li>
   <li>04/18/2022: Remade the script with 2022 standards</li>
+  <li>04/18/2022: Now the script prints header, content, and title if it's available.</li>
 </ul>
 </ul>
 
 <h3>How it works:</h3>
 <ul>
   <li>Run espn.py from anywhere you want.</li>
+</ul>
+
+<h3>Future:</h3>
+<ul>
+  <li>Top headlines will be also listed with relevant category in order to make a complete BottomLine.</li>
+  <li>A web application that can be included in bottom of a webpage</li>
+  <li>A GUI.</li>
 </ul>
--- espn.py
+++ espn.py
@@ -7,46 +7,56 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
+# Top headlines
+# http://www.espn.com/espn/latestnews
 # List of available leagues in ESPN database
+# also ncf and ncb included but for now discard
 leagues = ["nfl", "nba", "mlb", "nhl", "ncf", "ncb"]
+leaguedict = {"nfl": "NFL", "nba": "NBA", "mlb": "MLB", "nhl": "NHL", "ncf": "NCAAF", "ncb": "NCAAB"}
 
-for league in leagues:
-    url = f"http://www.espn.com/{league}/bottomline/scores"
+def parse(url):
     html = urllib.request.urlopen(url, context=ctx).read()
     soup = BeautifulSoup(html,'lxml')
+    return soup
+
+for league in leagues:
+    l = leaguedict.get(league)
+    url = f"http://www.espn.com/{league}/bottomline/scores"
+    soup = parse(url)
     x = urllib.parse.unquote(str(soup))
     # print(x)
     x = x.split("&amp")
-    for i in range(1,50):
+    for i in range(1,25):
         if f"left{i}" in str(x):
             left = [s.split("=")[1] for s in x if f"left{i}=" in s]
             right = [s.split("=")[1] for s in x if f"right{i}_" in s]
             right = right[:-1]
+            total = left + right
             link = [s.split(f"url{i}=")[1] for s in x if f"url{i}=" in s]
-            print(link)
-            gameId = link[0].split("gameId=")[-1]
+            if "preview" in link[0]:
+                soup = parse(link[0])
+            else:
+                gameId = link[0].split("gameId=")[-1]
+                recap = f"https://www.espn.com/{league}/recap/_/gameId/{gameId}"
+                soup = parse(recap)
             
-            recap = f"https://www.espn.com/{league}/recap/_/gameId/{gameId}"
             # print(recap)
             try:
-                html = urllib.request.urlopen(recap, context=ctx).read()
-                soup = BeautifulSoup(html,'lxml')
                 title = soup.find("header", {"class": ["article-header", "Story__Header"]})
                 title = title.find("h1")
                 u = title.string
+                total.append(u)
+                total.insert(0, l)
             except:
                 pass
                 try:
-                    recap = f"https://www.espn.com/{league}/game/_/gameId/{gameId}"
-                    html = urllib.request.urlopen(recap, context=ctx).read()
-                    soup = BeautifulSoup(html,'lxml')
                     title = soup.find("header", {"class": "top-stories__story-header"})
                     title = title.find("h1")
                     u = title.string
+                    total.append(u)
+                    total.insert(0, l)
                 except:
-                    u = "Not found"
+                    total.insert(0, l)
                     pass
-            print(left,right,u)
-            
 
-
+            print(total)
